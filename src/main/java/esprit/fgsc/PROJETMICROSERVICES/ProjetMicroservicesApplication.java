461be98d08dfc7144533b3f316fa2a523f746c60
ghadakhedri1 <ghada.khedri1@esprit.tn>
2021-10-27 23:00:39 +0100

    Swagger

--- src/main/java/esprit/fgsc/PROJETMICROSERVICES/ProjetMicroservicesApplication.java
+++ src/main/java/esprit/fgsc/PROJETMICROSERVICES/ProjetMicroservicesApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 @EnableEurekaClient
+@EnableSwagger2
+@SpringBootApplication
 public class ProjetMicroservicesApplication {
 
 	public static void main(String[] args) {
